f23e4ceb4b3dcfee257e10784d90372791199434
Rohan Patel <rpatel452@myseneca.ca>
2024-05-23 16:52:35 -0400

    lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: [Rohan Patel]
+# Author ID: [154338222]
+# Date Created: [2002/08/14]
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
