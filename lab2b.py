32e6f733b8801c56237e926669546c00c6d31df7
Rohan Patel <rpatel452@myseneca.ca>
2024-05-23 16:51:38 -0400

    lab2b.py

---
+++ lab2b.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Prompt the user for "Name: "
+name = input("Name: ")
+
+# Prompt the user for "Age: "
+age = input("Age: ")
+
+# Print the exact output as required
+print(f"Hi {name}, you are {age} years old.")
