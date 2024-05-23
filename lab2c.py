9b16f9d4f33784ffa295dcd6c635dbe9f0dfd631
Rohan Patel <rpatel452@myseneca.ca>
2024-05-23 16:52:08 -0400

    lab2c.py

---
+++ lab2c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Assigning the first argument to the string object named name
+name = sys.argv[1]
+
+# Assigning the second argument to the integer object named age
+age = int(sys.argv[2])
+
+# Print the exact output as required
+print("Hi {}, you are {} years old.".format(name, age))
+
